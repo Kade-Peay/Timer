02bd3ffc1d88ea2a185e25a88f89f314f7195ed1
kadepeay <kadepeay@gmail.com>
2020-06-03 16:51:35 -0600

    now works on a CLI using arguments

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a timer that when given an amount of seconds will count that down with the format
+00:00
+It will also not give a new line and will count down just from the same line.
--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
-import time
+import time, sys
 
 def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
+        print(timeformat, end='\r', flush=True)
         time.sleep(1)
         t -= 1
-    print('Goodbye!\n\n\n\n\n')
+    print('Goodbye!\n')
 
-countdown(120)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Not enough arguments')
+    else:
+        countdown(int(sys.argv[1]))
