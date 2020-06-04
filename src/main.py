9c1580e3226733f57f879394ef0667055aff8d76
kadepeay <kadepeay@gmail.com>
2020-06-03 22:08:48 -0600

    now raisess Type error

--- src/main.py
+++ src/main.py
@@ -14,4 +14,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('Not enough arguments')
     else:
-        countdown(int(sys.argv[1]))
+        try:
+            countdown(int(sys.argv[1]))
+        except:
+            raise TypeError
